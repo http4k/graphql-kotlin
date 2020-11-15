78854b2eec91c5a6fd98139116befd9664f876de
David Denton <denton.david@gmail.com>
2020-11-15 16:33:53 +0000

    add http4k version to properties

--- examples/http4k/build.gradle.kts
+++ examples/http4k/build.gradle.kts
@@ -8,8 +8,10 @@ application {
     mainClassName = "com.expediagroup.graphql.examples.http4k.ApplicationKt"
 }
 
+val http4kVersion: String by project
+
 dependencies {
-    implementation(platform("org.http4k:http4k-bom:3.276.0"))
+    implementation(platform("org.http4k:http4k-bom:$http4kVersion"))
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-graphql")
     implementation("com.expediagroup", "graphql-kotlin-schema-generator")
